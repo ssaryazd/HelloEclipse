c03133662b3f94257f4263d43b7828da1f91a189
Saeed Saryazdi Zadeh <saeed.saryazdi@gmail.com>
2021-11-21 07:51:43 -0800

    vesrion 2

--- HelloApp/src/HelloApp.java
+++ HelloApp/src/HelloApp.java
@@ -2,7 +2,7 @@
 public class HelloApp {
 
 	public static void main(String[] args) {
-		System.out.println("Saeed Saryazdi Zadeh");
+		System.out.println("Saeed Saryazdi Zadeh for Version 2 in different branch");
 
 	}
 
